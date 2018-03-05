44110e828aa545def4c712c501be1f1c8ed2da4e
Yannis Alexiou <johnmast7@yahoo.com>
2018-03-05 10:06:43 +0200

    Changing the Main View

--- FindACab/utils/Commons.js
+++ FindACab/utils/Commons.js
@@ -1,4 +1,6 @@
 Ext.define('Utils.Commons', {
+  //I can use statics from anywhere in my by calling
+  // Utils.Common.YELP_API
   statics: {
     YELP_API: 'http://api.yelp.com/business_review_search?',
     YELP_KEY: 'ftPpQUCgfSA3yV98-uJn9g',
@@ -7,7 +9,7 @@ Ext.define('Utils.Commons', {
 
     getUrl: function () {
       return this.YELP_API + "term=" + this.YELP_TERM +
-      "&ywsid=" + THIS.YELP_KEY + "&location=" + this.LOCATION; 
+      "&ywsid=" + THIS.YELP_KEY + "&location=" + this.LOCATION;
     }
   }
 })

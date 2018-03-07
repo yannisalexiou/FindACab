425beb39b11e5fc98302f2f1f3569f94695193be
Yannis Alexiou <johnmast7@yahoo.com>
2018-03-07 17:23:06 +0200

    Try to create dynamic config (comments)

--- FindACab/app/view/Main.js
+++ FindACab/app/view/Main.js
@@ -18,8 +18,30 @@ Ext.define('FindACab.view.Main', {
       }]
     },
 
-    initialize: function () {
-      console.log("Component Main initialized");
-      this.callParent();
-    }
+    // config: {
+    //   layout: 'hbox'
+    // },
+
+    // constructor: function (config) {
+    //   console.log("constructor");
+    //
+    //   var newItems = [{
+    //     xtype: 'overview',
+    //     flex: 1,
+    //     store: 'Cabs'
+    //   }, {
+    //     xtype: 'detailView',
+    //     flex: 3
+    //   }]
+    //
+    //   this.config['items'] = newItems;
+    //   console.log("Config: ", this.config);
+    //
+    //   this.callParent(arguments);
+    // },
+    //
+    // initialize: function () {
+    //   console.log("Component Main initialized");
+    //   this.callParent(arguments);
+    // }
 });
